c2171e1519b1fa5613ac300bc67169141072b642
rileymanda1@gmail.com <rileymanda1@gmail.com>
2020-03-17 03:11:11 +0530

    Website Update2

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,10 +15,10 @@ $(function () {
         var scroll = $(window).scrollTop();
         if (scroll < 20) {
             $(".navbar-area").removeClass("sticky");
-            $(".navbar .navbar-brand img").attr("src", "assets/images/logo.svg");
+            $(".navbar .navbar-brand img").attr("src", "assets/images/logowhiteh.svg");
         } else {
             $(".navbar-area").addClass("sticky");
-            $(".navbar .navbar-brand img").attr("src", "assets/images/logo-2.svg");
+            $(".navbar .navbar-brand img").attr("src", "assets/images/logoh.svg");
         }
     });
 
